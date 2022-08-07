89a80e18123aef50acc8ed2c4ccd3980cd1b7574
Yudong Sun <yudong.sun@mpq.mpg.de>
2022-08-07 17:45:18 +0200

    anritsu test successful

---
+++ suny/combination/anritsu_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import pyvisa
+import time
+
+rm = pyvisa.ResourceManager()
+anritsu_session = rm.open_resource("USB0::0x0B5B::0xFFF9::2144030_1768_55::INSTR") # Anritsu MS2720T
+
+anritsu_session.write("DISP:WIND:TRAC:Y:SCAL:RLEV 30dBm")
+anritsu_session.write("CALC:MARK1:MAX")
+anritsu_session.write("CALC:MARK1:Y?")
+power_level = anritsu_session.read()
+print(power_level)
+
+anritsu_session.close()
+rm.close()
+
