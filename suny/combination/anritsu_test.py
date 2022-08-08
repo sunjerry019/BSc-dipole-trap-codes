292f47f2dd434d8631f2ff02e0a4bc79d1949756
Yudong Sun <yudong.sun@mpq.mpg.de>
2022-08-08 10:31:15 +0200

    amplfier mapping code

--- suny/combination/anritsu_test.py
+++ suny/combination/anritsu_test.py
@@ -9,8 +9,13 @@ anritsu_session = rm.open_resource("USB0::0x0B5B::0xFFF9::2144030_1768_55::INSTR
 anritsu_session.write("DISP:WIND:TRAC:Y:SCAL:RLEV 30dBm")
 anritsu_session.write("CALC:MARK1:MAX")
 anritsu_session.write("CALC:MARK1:Y?")
+
 power_level = anritsu_session.read()
 print(power_level)
+
+anritsu_session.write("CALC:MARK1:X?")
+f = float(anritsu_session.read())/1e6
+print(f)
 
 anritsu_session.close()
 rm.close()
