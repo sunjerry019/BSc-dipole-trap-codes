861de78676a9c1cd803d0cb88f230be6eda19cb7
Yudong Sun <yudong.sun@physik.uni-muenchen.de>
2022-08-10 09:59:52 +0200

    fix bandwidth evals using eval_uncertainty

--- suny/spectrum_analyser/AOM-Driver-Bandwidth.py
+++ suny/spectrum_analyser/AOM-Driver-Bandwidth.py
@@ -16,7 +16,10 @@ import uncertainties
 
 ## SETTINGS
 nrows = 4; ncols = 1
-figwidth = 5.5; figheight = 6
+figwidth = 5.5; figheight = 6.5
+bw_figwidth = 6; bw_figheight = 5
+
+SKIP_4_BW_PLOT = True
 
 # rolcol(2,2), figsize(8,4)
 multiplot = False
@@ -52,9 +55,13 @@ keys.sort(key = getNumFromKey)
 
 models  = {}
 results = {}
+BW_lefts   = {}
+BW_rights  = {}
+bandwidths = {}
 
 for i, key in enumerate(keys):
     khz = getNumFromKey(key)
+    print(khz, "Khz =============")
 
     freqs = sweeping_freqs[key].DATA["A"]["FREQ"]
     power = sweeping_freqs[key].DATA["A"]["POWER"]
@@ -133,9 +140,11 @@ for i, key in enumerate(keys):
         return _result.eval(x = x) - minus3db.nominal_value
 
     def eval_func_top(x: np.ndarray | float) -> np.ndarray | float:
-        return _result.eval(x = x) - minus3db.nominal_value + minus3db.std_dev
+        del_y = _result.eval_uncertainty(x = x)
+        return _result.eval(x = x) + del_y - minus3db.nominal_value +  + minus3db.std_dev
     def eval_func_bottom(x: np.ndarray | float) -> np.ndarray | float:
-        return _result.eval(x = x) - minus3db.nominal_value - minus3db.std_dev
+        del_y = _result.eval_uncertainty(x = x)
+        return _result.eval(x = x) - del_y - minus3db.nominal_value - minus3db.std_dev
 
     # SOME ROOTFINDING ALGOS here
     _left_res  = scipy.optimize.root(fun = eval_func, x0 = center1)
@@ -152,55 +161,86 @@ for i, key in enumerate(keys):
     _left = uncertainties.ufloat(_left_res.x, _left_res_dev)
     _right = uncertainties.ufloat(_right_res.x, _right_res_dev)
 
-    bandwidth = _right - _left
+    bandwidth = np.abs(_right - _left)
+    
+    BW_lefts[key] = _left
+    BW_rights[key] = _right
+    bandwidths[key] = bandwidth
 
-    print("bandwidth", bandwidth, "dBm")
+    print("bandwidth", bandwidth, "MHz\n")
 
-    bandwidth_plot_x = np.array([_left.nominal_value, _right.nominal_value])
-    bandwidth_plot_y = _result.eval(x = bandwidth_plot_x)
+    # bandwidth_plot_x = np.array([_left.nominal_value, _right.nominal_value])
+    # bandwidth_plot_x_err = np.array([_left.std_dev, _right.std_dev])
+    # bandwidth_plot_y = _result.eval(x = bandwidth_plot_x)
 
-    _freqs_fit = np.linspace(np.min(freqs), np.max(freqs), 1000)
-    plt.scatter(freqs, power, label='data', marker = '+')
-    plt.scatter(bandwidth_plot_x, bandwidth_plot_y, label = "Bandwidth", marker = "x")
-    plt.plot(_freqs_fit, _result.eval(x = _freqs_fit), 'r-', label='interpolated fit')
-    plt.legend()
-    plt.show()
-    # plt.savefig(f'./tv1/peak_{np.around(p, decimals = 5)}.eps', format='eps')
-    plt.clf()
-
-sys.exit()
+    # _freqs_fit = np.linspace(np.min(freqs), np.max(freqs), 1000)
+    # plt.scatter(freqs, power, label='data', marker = '+')
+    # plt.errorbar(x = bandwidth_plot_x, y = bandwidth_plot_y, xerr = bandwidth_plot_x_err, label = "Bandwidth", uplims=True, lolims=True, ecolor = "red", color = "green", capsize = 10)
+    # plt.plot(_freqs_fit, _result.eval(x = _freqs_fit), 'r-', label='interpolated fit')
+    # plt.legend()
+    # plt.show()
+    # # plt.savefig(f'./tv1/peak_{np.around(p, decimals = 5)}.eps', format='eps')
+    # plt.clf()
 
 # PLOTTING BANDWIDTH
-keys_for_plotting = ['1kHz', '50kHz', '100kHz-30kHzBW', '400kHz-100kHzBW']
-if multiplot:
-    fig, axs = plt.subplots(nrows = nrows, ncols = ncols, sharex = 'col', sharey = 'row', squeeze = False, figsize=(figwidth, figheight))
+if not SKIP_4_BW_PLOT:
+    keys_for_plotting = ['1kHz', '50kHz', '100kHz-30kHzBW', '400kHz-100kHzBW']
+    if multiplot:
+        fig, axs = plt.subplots(nrows = nrows, ncols = ncols, sharex = 'col', sharey = 'row', squeeze = False, figsize=(figwidth, figheight))
 
-    for i, key in enumerate(keys_for_plotting):
-        data = sweeping_freqs[key]
-        ax = axs[i // ncols, i % ncols]
-        ax.scatter(data.DATA["A"]["FREQ"], data.DATA["A"]["POWER"], marker = "+", label = key)
-        ax.set_title(key)
-        ax.set_xlim([70,89])
+        for i, key in enumerate(keys_for_plotting):
+            data = sweeping_freqs[key]
+            ax = axs[i // ncols, i % ncols]
 
-    fig.supxlabel("Frequency (MHz)")
-    fig.supylabel("Power after attenuation (dBm)")
-else:
-    fig, ax = plt.subplots(figsize = (figwidth, figheight))
-    l = len(keys)
-    for i, key in enumerate(keys_for_plotting):
-        j = l - i
-        data = sweeping_freqs[key]
+            ax.scatter(data.DATA["A"]["FREQ"], data.DATA["A"]["POWER"], marker = "+", label = key)
+            ax.set_title(key)
+            ax.set_xlim([70,89])
 
-        # PLOT THE DATA
-        ax.scatter(data.DATA["A"]["FREQ"], data.DATA["A"]["POWER"] + j*offset, marker = "+", label = f"{getNumFromKey(key)} kHz Modulation")
+        fig.supxlabel("Frequency (MHz)")
+        fig.supylabel("Power after attenuation (dBm)")
+    else:
+        fig, ax = plt.subplots(figsize = (figwidth, figheight))
+        l = len(keys_for_plotting)
 
-    ax.set_xlabel("Frequency (MHz)")
-    ax.set_ylabel("Power after attenuation (dBm)")
-    fig.suptitle("\\shortstack{Output RF Bandwidth from POS-150+ AOM Driver\\\\at different modulation frequencies}")
+        fit_x = np.linspace(start = 70, stop = 90, num = 1000, endpoint = True)
+        colors = ['darkturquoise', 'burlywood', 'mediumseagreen', 'lightcoral']
 
-    ax.legend(loc='lower center', bbox_to_anchor = (0.5, -0.3), ncol=2, fancybox=True)
-    fig.subplots_adjust(bottom=0.25) # https://www.adamsmith.haus/python/answers/how-to-place-a-legend-below-the-axes-in-matplotlib-in-python
-    plt.xlim([70,89])
+        for i, key in enumerate(keys_for_plotting):
+            j = l - i
+            data = sweeping_freqs[key]
 
-plt.show()
+            _LR = [BW_lefts[key], BW_rights[key]]
+            bandwidth_plot_x     = np.array([x.nominal_value for x in _LR])
+            bandwidth_plot_x_err = np.array([x.std_dev for x in _LR])
+            bandwidth_plot_y     = results[key].eval(x = bandwidth_plot_x) + j*offset
+
+            fit_y = results[key].eval(x = fit_x) + j*offset
+            fit_y_delta = results[key].eval_uncertainty(x = fit_x)
+
+            # PLOT THE DATA
+            ax.scatter(data.DATA["A"]["FREQ"], data.DATA["A"]["POWER"] + j*offset, marker = ".", label = f"{getNumFromKey(key)} kHz Modulation", color = colors[i], s = 6)
+            ax.fill_between(fit_x, fit_y - fit_y_delta, fit_y + fit_y_delta, color='#888888')
+            if i == 1:
+                ax.plot(fit_x, fit_y, color = "tab:red", label = "erf-Rectangle Fit")
+                ax.errorbar(x = bandwidth_plot_x, y = bandwidth_plot_y, xerr = bandwidth_plot_x_err, capsize = 5, ecolor = "black", color = "black", marker = "x", label = "3 dB Bandwidth")
+            else:
+                ax.plot(fit_x, fit_y, color = "tab:red")
+                ax.errorbar(x = bandwidth_plot_x, y = bandwidth_plot_y, xerr = bandwidth_plot_x_err, capsize = 5, ecolor = "black", color = "black", marker = "x")
+
+        ax.set_xlabel("Frequency (MHz)")
+        ax.set_ylabel("Power after attenuation (dBm)")
+        fig.suptitle("\\shortstack{Output RF Bandwidth from POS-150+ AOM Driver\\\\at different modulation frequencies}")
+
+        ax.legend(loc='lower center', bbox_to_anchor = (0.5, -0.35), ncol=2, fancybox=True)
+        fig.subplots_adjust(bottom=0.25) # https://www.adamsmith.haus/python/answers/how-to-place-a-legend-below-the-axes-in-matplotlib-in-python
+        plt.xlim([70,89])
+
+    plt.show()
+    plt.clf()
+
+# PLOTTING THE BANDWIDTH vs FREQ
+BW_freqs = np.array([getNumFromKey(k) for k in keys]) # in kHz
+BW_bandwidths = np.array([bandwidths[k].nominal_value for k in keys]) # in MHz
+BW_bandwidths_err = np.array([bandwidths[k].std_dev for k in keys]) # in MHz
+
 
