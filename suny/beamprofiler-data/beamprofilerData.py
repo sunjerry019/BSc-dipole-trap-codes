5866a0a032122997d1c6d4fb832e322655482280
Yudong Sun <yudong.sun@physik.uni-muenchen.de>
2022-08-10 15:14:36 +0200

    prepare for final figure for sweeping

---
+++ suny/beamprofiler-data/beamprofilerData.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+class BeamprofilerImageData():
+    def __init__(self) -> None:
+        pass
+
+class BeamprofilerTimeseriesData():
+    def __init__(self) -> None:
+        pass
+
---
+++ suny/final_sweeping.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from anritsuData import AnritsuData
+import os, glob
+
+from matplotlib import rc
+import matplotlib.pyplot as plt
