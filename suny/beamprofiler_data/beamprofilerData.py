c51d46a0526102f1007335e9df21f929188232ab
Yudong Sun <yudong.sun@physik.uni-muenchen.de>
2022-08-11 08:17:30 +0200

    reorganize

--- suny/beamprofiler_data/beamprofilerData.py
+++ suny/beamprofiler_data/beamprofilerData.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import numpy._typing as np_t
 import matplotlib.pyplot as plt
 import scipy.ndimage
 
+import sys
+
 class BeamprofilerImageData():
-    def __init__(self, datafile: str, autorotate: bool = True) -> None:
+    def __init__(self, datafile: str, autocrop: bool = True, autorotate: bool = True) -> None:
         self.DATA = np.loadtxt(datafile, delimiter = ',')
-        
+        self.ORIGINAL_DATA = self.DATA
+
+        if autocrop:
+            self.cropToContent()
+
         if autorotate:
             self.rotateImage(degrees = self.getAngleForRotation())
 
+        # MAYBE at some point: https://stackoverflow.com/questions/45177154/how-to-decode-color-mapping-in-matplotlibs-colormap
+
+    def cropToContent(self, padding: int = 5, threshold: float = 2) -> None:
+        # CITE: https://stackoverflow.com/a/48987831
+
+        coords = np.argwhere(self.DATA > threshold)
+        x_min, y_min = coords.min(axis=0)
+        x_max, y_max = coords.max(axis=0)
+
+        x_min -= padding; y_min -= padding
+        x_max += padding; y_max += padding
+
+        self.DATA = self.DATA[x_min:x_max+1, y_min:y_max+1]
+
+        # with np.printoptions(threshold=sys.maxsize):
+        #     print(self.DATA)
+
     def getAngleForRotation(self) -> float:
         # To obtain the angle, we use https://alyssaq.github.io/2015/computing-the-axes-or-orientation-of-a-blob/#2-Raw-image-moments
+        # https://stackoverflow.com/questions/9005659/compute-eigenvectors-of-image-in-python
         # Write something about PCA and moments and the equation
         
         cov = BeamprofilerImageData.moments_cov(self.DATA)
@@ -21,18 +46,29 @@ class BeamprofilerImageData():
         largest_eig_idx = np.argmax(eigvals)
         # We only look at the eigenvector with the largest eigenvalue
         _x, _y = eigvecs[largest_eig_idx]
-        theta = np.rad2deg(np.arctan(_x/_y)) + 90 + 2 # small adjustment
+        theta = np.rad2deg(np.arctan(_x/_y)) + 90 # small adjustment
 
         return theta
 
     def rotateImage(self, degrees: float) -> None:
-        self.ORIGINAL_DATA = self.DATA
         self.DATA = scipy.ndimage.rotate(self.DATA, angle = degrees)
     
     def displayImage(self) -> None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
         ax.imshow(self.DATA)
         plt.show()
+    
+    def padToSize(self, shape: np_t._ShapeLike) -> None:
+        biggerImage = np.zeros(shape = shape)
+
+        n_x, n_y = np.shape(biggerImage)
+        o_x, o_y = np.shape(self.DATA)
+
+        left = (n_x - o_x) // 2
+        top  = (n_y - o_y) // 2
+
+        biggerImage[left:left+o_x, top:top+o_y] = self.DATA
+        self.DATA = biggerImage
 
     ## AUXILLARY METHODS
     @staticmethod
@@ -62,5 +98,7 @@ class BeamprofilerTimeseriesData():
 if __name__ == "__main__":
     import os
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    x = BeamprofilerImageData(datafile = os.path.join(base_dir, "./2022-08-05_High Frequency Painting/100kHz_+-1.250.0V_10MHzModulation.csv"))
+    x = BeamprofilerImageData(datafile = os.path.join(base_dir, "/mnt/data/School/University/Deutschland/LMU/Modules/1_PH/2022SS/Bachelorarbeit/dipole-trap-codes/suny/beamprofiler_data/2022-08-05_High Frequency Painting/100kHz_1.250.0V_10MHzModulation.csv"))
+    # x.cropToContent()
+    # x.padToSize((500, 500))
     x.displayImage()
