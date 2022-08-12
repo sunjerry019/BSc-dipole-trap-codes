fad0bf8e6a7bea81b129cb6681b8b80750f999c9
Yudong Sun <yudong.sun@physik.uni-muenchen.de>
2022-08-12 18:14:25 +0200

    reorder legend functions

--- suny/plotter.py
+++ suny/plotter.py
@@ -5,10 +5,13 @@
 from matplotlib import rc
 import matplotlib.pyplot as plt
 
+import matplotlib.patches as mpl_p
+import matplotlib.legend as mpl_l
+
 from matplotlib.axes._axes import Axes
 from matplotlib.ticker import AutoMinorLocator
 
-from typing import Tuple
+from typing import Any, Iterable, Tuple
 
 import numpy as np
 
@@ -39,7 +42,76 @@ class Plotter():
             self.ax.tick_params(axis = 'both', which = 'minor', colors = self.MINORTICK_COLOR)
             self.ax.xaxis.set_minor_locator(AutoMinorLocator())
             self.ax.yaxis.set_minor_locator(AutoMinorLocator())
-            
+    
+
+    #  Returns tuple of handles, labels for axis ax, after reordering them to conform to the label order `order`, and if unique is True, after removing entries with duplicate labels.
+    # https://stackoverflow.com/a/35926913
+    @staticmethod
+    def reorderLegend(ax: Axes | None = None, order: list | None = None, unique: bool = False) -> Tuple[Tuple, Tuple]:
+        if ax is None: 
+            ax = plt.gca()
+
+        handles, labels = ax.get_legend_handles_labels()
+        labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0])) # sort both labels and handles by labels
+        if order is not None: # Sort according to a given list (not necessarily complete)
+            keys=dict(zip(order,range(len(order))))
+            labels, handles = zip(*sorted(zip(labels, handles), key=lambda t,keys=keys: keys.get(t[0], np.inf)))
+        
+        if unique:  
+            labels, handles= zip(*Plotter.unique_everseen(zip(labels,handles), key = labels)) # Keep only the first of each handle
+        
+        legendProp = { "handles": handles, "labels": labels}
+
+        # Check if legend already exists
+        oldLegend = ax.get_legend()
+        # if old legend exists, update
+        if oldLegend is not None:
+            legendProp = Plotter.updateLegend(oldLegend = oldLegend, **legendProp)
+        
+        ax.legend(**legendProp)
+        return (handles, labels)
+
+    @staticmethod
+    def updateLegend(oldLegend: mpl_l.Legend, **kwargs) -> dict:
+        # https://stackoverflow.com/a/28089529
+
+        defaults = dict(
+            loc = oldLegend._loc,
+            numpoints = oldLegend.numpoints,
+            markerscale = oldLegend.markerscale,
+            scatterpoints = oldLegend.scatterpoints,
+            scatteryoffsets = oldLegend._scatteryoffsets,
+            prop = oldLegend.prop,
+            # fontsize = None,
+            borderpad = oldLegend.borderpad,
+            labelspacing = oldLegend.labelspacing,
+            handlelength = oldLegend.handlelength,
+            handleheight = oldLegend.handleheight,
+            handletextpad = oldLegend.handletextpad,
+            borderaxespad = oldLegend.borderaxespad,
+            columnspacing = oldLegend.columnspacing,
+            ncol = oldLegend._ncol,
+            mode = oldLegend._mode,
+            fancybox = type(oldLegend.legendPatch.get_boxstyle()) == mpl_p.BoxStyle.Round,
+            shadow = oldLegend.shadow,
+            title = oldLegend.get_title().get_text() if oldLegend._legend_title_box.get_visible() else None,
+            framealpha = oldLegend.get_frame().get_alpha(),
+            bbox_to_anchor = oldLegend.get_bbox_to_anchor()._bbox,
+            bbox_transform = oldLegend.get_bbox_to_anchor()._transform,
+            frameon = oldLegend.draw_frame,
+            handler_map = oldLegend._custom_handler_map,
+        )
+
+        if "fontsize" in kwargs and "prop" not in kwargs:
+            defaults["prop"].set_size(kwargs["fontsize"])
+
+        return defaults | kwargs # merge the two dictionaries
+
+    @staticmethod
+    def unique_everseen(seq: Iterable, key: Iterable | None = None) -> list[Any]:
+        seen = set()
+        seen_add = seen.add
+        return [x for x,k in zip(seq,key) if not (k in seen or seen_add(k))]
 
     @staticmethod
     def initMPLSettings():
@@ -47,6 +119,7 @@ class Plotter():
         rc('text.latex', preamble = r"\usepackage{libertine}\usepackage{nicefrac}")
         rc('font', size = 11, family = "Sans-Serif")
 
+    # MAPPING FUNCS
     def xlim(self, *args, **kwargs) -> Tuple[float, float]:
         return plt.xlim(*args, **kwargs)
 
